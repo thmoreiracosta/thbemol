8772d6ce7c5457b22ea19df22571ed71e76ba8ce
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-26 01:31:36 -0300

    Header finalizado layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,22 @@
 import logo from '../../assets/img/ContaBemol-b 1.png';
 import celular from '../../assets/img/Phone-Spotlight-Desktop 1.png';
+import './style.css'
+
 
 export default function Header() {
   return (
     <div>
-      <header>
+      <header className='container-header'>
         <img src={logo} alt="logo Bemol cabeçalho" />
       </header>
-      <main>
+      <main className="container-main">
         <div>
           <h1>Chegou a conta digital da Bemol</h1>
           <p>
             Pague parcelas Bemol pelo celular, deposite nas Lojas Bemol, Bemol
             Farma, Mercado Bemol e acumule muitos pontos de bônus!
           </p>
-          <a href="https://www.bemol.com.br/contabemol" target="_new">Abra sua conta Bemol</a>
+          <a className="button" href="https://www.bemol.com.br/contabemol" target="_new">Abra sua conta Bemol</a>
         </div>
         <div>
           <img src={celular} alt="smartphone" />
