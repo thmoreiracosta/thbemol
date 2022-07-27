020777c40e2dbc11a62df5406fcaa8472b147155
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-27 10:28:29 -0300

    section One complete

--- src/components/SectionOne/SectionOne.tsx
+++ src/components/SectionOne/SectionOne.tsx
@@ -10,43 +10,40 @@ import './style.css'
 export default function SectionOne () {
   return (
     <>
-      <div className='container-section'>
-        <div className='container-regina'>
-          <h2>
-            A Conta Bemol é a conta digital que traz benefícios exclusivos para
-            o cliente Bemol.
-          </h2>
-
-          <img id='regina' src={regina} alt='Dona Regina sorrindo' />
-        </div>
-        <div className='container-pontos'>
-          <div>
-            <div>
-              <img src={vg1} alt='pontos acumulados' />
-              <span>Acúmulo de pontos de bônus</span>
-            </div>
-            <div>
-              <img src={vg2} alt='atendimento presencial' />
-              <span>Atendimento nas Lojas</span>
-            </div>
-            <div>
-              <img src={vg3} alt='pagamento parcelado' />
-              <span>Pagamento de parcelas Bemol</span>
-            </div>
+      <div className='background'>
+        <div className='container-section'>
+          <div className='container-regina'>
+            <h2>
+              A Conta Bemol é a conta digital que traz benefícios exclusivos
+              para o cliente Bemol.
+            </h2>
+            <img id='regina' src={regina} alt='Dona Regina sorrindo' />
           </div>
+          <div className='container-pontos'>
+            <div className='container-icons'>
+              <img src={vg1} alt='pontos acumulados' />
+              <em>Acúmulo de pontos de bônus</em>
+            </div>
+            <div className='container-icons'>
+              <img src={vg2} alt='atendimento presencial' />
+              <em>Atendimento nas Lojas</em>
+            </div>
+            <div className='container-icons'>
+              <img src={vg3} alt='pagamento parcelado' />
+              <em>Pagamento de parcelas Bemol</em>
+            </div>
 
-          <div>
-            <div>
+            <div className='container-icons'>
               <img src={vg4} alt='fazer depósito' />
-              <span>Depósito nas Lojas Bemol e Bemol Farma</span>
+              <em>Depósito nas Lojas Bemol e Bemol Farma</em>
             </div>
-            <div>
+            <div className='container-icons'>
               <img src={vg5} alt='anuidade zero' />
-              <span>Zero anuidade</span>
+              <em>Zero anuidade</em>
             </div>
-            <div>
+            <div className='container-icons'>
               <img src={vg6} alt='transferia via pix' />
-              <span>Transferência via PIX</span>
+              <em>Transferência via PIX</em>
             </div>
           </div>
         </div>
